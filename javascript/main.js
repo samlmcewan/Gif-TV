a5fbb84a0b5fc0fd1d1499a8cd1297a42433b1f5
samlmcewan <samlmcewan@gmail.com>
2020-04-20 00:13:15 +0100

    initial upload from gif search engine project

---
+++ javascript/main.js
@@ -0,0 +1,44 @@
+// Get the input value
+document.querySelector("#form").addEventListener('submit',function(e){
+  var input = document.querySelector("input").value;
+  pushToDOM(input);
+  
+  e.preventDefault();
+});
+
+// Process the data 
+var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+
+// AJAX request
+var AJAXGiphyCall = new XMLHttpRequest();
+
+AJAXGiphyCall.open( 'GET', url );
+AJAXGiphyCall.send();
+
+AJAXGiphyCall.addEventListener('load', function(e){
+	 var data = e.target.response;
+	 pushToDOM(data);
+
+});
+
+
+
+// Display the input
+function pushToDOM(input) {
+
+	// cleans up data into an object
+	var response = JSON.parse(input);
+
+	var imageURLs = response.data; 
+	var container = document.querySelector(".js-container");
+
+	imageURLs.forEach(function(image){
+
+		var src = image.images.fixed_width.url;
+		
+  		container.innerHTML += "<img src='" + src + "'class='container-image'>";
+
+	});
+
+	// displays input in html container
+}
